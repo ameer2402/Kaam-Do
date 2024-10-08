5a0906bf002e802eff7f158bc820c2927aee2ca1
MOHAMMED AMEERKHAN <130216857+ameer2402@users.noreply.github.com>
2024-10-08 16:15:49 +0530

    Delete server directory

--- server/server/routes/router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {createFeedback} from "../controllers/feedback.controller.js";
-
-const router = express.Router();
-
-// Feedback Router
-router.post("/createFeedback", createFeedback);
-
-export default router;
